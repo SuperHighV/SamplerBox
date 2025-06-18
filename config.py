5a9dac7d834fdc5bfd2d50f9f46d3733b206b333
SuperHighV <44472691+SuperHighV@users.noreply.github.com>
2025-06-18 17:10:14 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 #########################################
 
 AUDIO_DEVICE_ID = 1                     # change this number to use another soundcard
-SAMPLES_DIR = "/media/"                       # The root directory containing the sample-sets. Example: "/media/" to look for samples on a USB stick / SD card
+SAMPLES_DIR = "/MD/"                       # The root directory containing the sample-sets. Example: "/media/" to look for samples on a USB stick / SD card
 MAX_POLYPHONY = 80                      # This can be set higher, but 80 is a safe value
 USE_BUTTONS = False                     # Set to True to use momentary buttons (connected to RaspberryPi's GPIO pins) to change preset
 USE_I2C_7SEGMENTDISPLAY = False         # Set to True to use a 7-segment display via I2C
